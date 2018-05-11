16f91680346d0280bf2ccae1746fa848e3e05735
wangxueming <29136104+eussi@users.noreply.github.com>
2018-05-11 15:35:59 +0800

    add demand

--- getTranFileList.sh
+++ getTranFileList.sh
@@ -14,6 +14,15 @@ function printTran() {
 	echo -e $1 >> ./temp/configs.txt
 }
 
+#################################################
+#(函数)返回执行命令的交易码
+#arg1 传入交易码
+#################################################
+function returnResult() {
+	cat $1 | grep $2 >/dev/null
+	echo $?
+}
+
 #得到ESB部署路径
 deployPath=$(getConfigs deploy.path)
 print "ESB部署路径:"
@@ -28,6 +37,7 @@ print "开始生成配置文件列表文件:./temp/configs.txt"
 > ./temp/configs.txt
 printTran "#交易文件列表"
 #循环交易码
+print "[IN_SERVECE IN_PARSER IN_PACKER OUT_SERVICE OUT_PARSER OUT_PACKER]"
 for tranCode in $(cat ./configs/tranCode.txt | grep -v "#" | grep -v "^$")
 do	
 	code=$(echo $tranCode | grep ":" | cut -d ":" -f 1)
@@ -39,15 +49,61 @@ do
 		exit 1
 	fi
 	#生成路径,紫金农商银行业务规则
-	printTran "#交易码:$code"
+	#printTran "#交易码:$code"
 	#in端
-	printTran "${deployPath}/configs/in_conf/metadata/define/service_${code}.xml"
-	printTran "${deployPath}/configs/in_conf/metadata/${channel}/channel_${channel}_service_${code}.xml"
-	printTran "${deployPath}/configs/in_conf/metadata/${channel}/service_${code}_system_${channel}.xml"
-	#out端
-	printTran "${deployPath}/configs/out_conf/metadata/${service}/service_${code}.xml"
-	printTran "${deployPath}/configs/out_conf/metadata/${service}/channel_${service}_service_${code}.xml"
-        printTran "${deployPath}/configs/out_conf/metadata/${service}/service_${code}_system_${service}.xml"
+	inService=${deployPath}/configs/in_conf/metadata/define/service_${code}.xml
+	inParser=${deployPath}/configs/in_conf/metadata/${channel}/channel_${channel}_service_${code}.xml
+	inPacker=${deployPath}/configs/in_conf/metadata/${channel}/service_${code}_system_${channel}.xml
+	outService=${deployPath}/configs/out_conf/metadata/${service}/service_${code}.xml
+	outParser=${deployPath}/configs/out_conf/metadata/${service}/channel_${service}_service_${code}.xml
+	outPacker=${deployPath}/configs/out_conf/metadata/${service}/service_${code}_system_${service}.xml
+	#打印生成消息
+	print "文件生成图："
+	pr="[    "
+	#判断是否已经存在
+	result=$(returnResult ./temp/configs.txt inService)
+	if [ "0" != ${result} ]; then
+		printTran ${inService}
+		pr="${pr}O          "
+	else
+		pr="${pr}X          "
+	fi
+	result=$(returnResult ./temp/configs.txt inParser)
+	if [ "0" != ${result} ]; then
+		printTran ${inParser}
+		pr="${pr}O         "
+	else
+		pr="${pr}X         "
+	fi
+	result=$(returnResult ./temp/configs.txt inPacker)
+	if [ "0" != ${result} ]; then
+		printTran ${inPacker}
+		pr="${pr}O          "
+	else
+		pr="${pr}X          "
+	fi
+	result=$(returnResult ./temp/configs.txt outService)
+	if [ "0" != ${result} ]; then
+		printTran ${outService}
+		pr="${pr}O          "
+	else
+		pr="${pr}X          "
+	fi
+	result=$(returnResult ./temp/configs.txt outParser)
+	if [ "0" != ${result} ]; then
+		printTran ${outParser}
+		pr="${pr}O          "
+	else
+		pr="${pr}X          "
+	fi
+	result=$(returnResult ./temp/configs.txt outPacker)
+	if [ "0" != ${result} ]; then
+		printTran ${outPacker}
+		pr="${pr}O    ]"
+	else
+		pr="${pr}X    ]"
+	fi
+	print ${pr}
 	print "交易码[$code]已正确生成配置文件"
 done
 
